15709687699251dd2358d7b009f4bc9e8ea6a987
sun win7 notebook <32594416+sun1218@users.noreply.github.com>
2018-10-08 22:45:48 +0800

    更改文件存储方式，完成2篇
    由于Dev C++ 的项目管理有一定的缺陷，在执行中可能会出现main function多次创建的问题，所以选择使用文件夹管理。

---
+++ ch0101/ch0101_02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+int main(void)
+{
+	long long1,long2,long3;
+	scanf("%d%d%d", &long1,&long2,&long3);
+	printf("%d",long2);
+	return 0;
+}
---
+++ ch0101/ch0101_03.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int int1,int2,int3;
+	scanf("%d%d%d", &int1,&int2,&int3);
+	printf("%8d%8d%8d",int1,int2,int3);
+	return 0;
+}
+
